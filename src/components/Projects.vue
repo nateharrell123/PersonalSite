873adf66b2a8fc2883c02c9cae9b958390fabd62
Nate Harrell <54548830+nateharrell123@users.noreply.github.com>
2022-07-10 13:15:39 -0500

    grid3

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -3,21 +3,23 @@
         <v-col cols="3"/> <!-- left -->
         <v-col cols="2">
             <v-card>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi minima placeat natus ut recusandae odio dolorem ullam corrupti veritatis vero. Consequatur unde dolorem natus, libero accusamus eveniet fugiat atque dolores!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, maxime ab, nesciunt ipsum sunt, perspiciatis voluptatibus quo quae ipsam ut quis aliquid. Officiis, nam tempore cupiditate vel error officia voluptatibus.
             </v-card>
         </v-col>
         <v-col cols="2">
             <v-card>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque asperiores placeat et laboriosam numquam nostrum veniam odit error aliquid animi corporis debitis, molestias ullam nulla ea beatae eum modi alias.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, maxime ab, nesciunt ipsum sunt, perspiciatis voluptatibus quo quae ipsam ut quis aliquid. Officiis, nam tempore cupiditate vel error officia voluptatibus.
             </v-card>
         </v-col>
         <v-col cols="2">
             <v-card>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi minima placeat natus ut recusandae odio dolorem ullam corrupti veritatis vero. Consequatur unde dolorem natus, libero accusamus eveniet fugiat atque dolores!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, maxime ab, nesciunt ipsum sunt, perspiciatis voluptatibus quo quae ipsam ut quis aliquid. Officiis, nam tempore cupiditate vel error officia voluptatibus.
             </v-card>
         </v-col>
+        
         <v-col cols="3"/> <!-- right -->
     </v-row>
+    
 </template>
 
 <script>
