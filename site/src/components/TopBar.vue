3c3be3a290d644014b39ab0bb712781f5907d68a
nate <nateharrell@ksu.edu>
2022-08-23 13:40:03 -0500

    hi

---
+++ site/src/components/TopBar.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <v-app-bar>
+            <v-row>
+                <v-col cols="3"> Col 1 </v-col>
+                <v-col cols="3"> Col 2 </v-col>
+                <v-col cols="3"> Col 3 </v-col>
+                <v-col cols="3"> Col 4 </v-col>
+            </v-row>
+        </v-app-bar>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "TopBar",
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ site/src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+  <TopBar />
+  <HomePage />
+</div>
+</template>
+
+<script>
+import HomePage from "../components/HomePage";
+import TopBar from "../components/TopBar"
+export default {
+  name: "Home",
+
+  components: {
+    HomePage,
+    TopBar
+  },
+};
+</script>
