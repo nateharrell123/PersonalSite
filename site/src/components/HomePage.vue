33ae09227cc74f0cd3d9ed3a47c3a714ef263efd
nate <nateharrell@ksu.edu>
2022-08-24 20:06:48 -0500

    tutorial part 1

--- site/src/components/HomePage.vue
+++ site/src/components/HomePage.vue
@@ -1,6 +1,18 @@
 <template>
-  <div>
-    HomePage
+  <div class="wrapper">
+    <h2>HomePage</h2>
+    <div class="box">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
   </div>
 </template>
 
@@ -12,3 +24,17 @@ export default {
   }),
 };
 </script>
+
+<style scoped>
+body{
+  margin: 0;
+  padding: 0;
+}
+.wrapper{
+  position: absolute;
+  width: 100%;
+  height:100%;
+  overflow:hidden;
+  background:linear-gradient(90deg, rgba(2,0,.6,1) 0%, rgba(16, 124, 146,1));
+}
+</style>
