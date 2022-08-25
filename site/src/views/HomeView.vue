b0e2e17633885edddeec309fae3bc2bf66679d2a
nate <nateharrell@ksu.edu>
2022-08-24 20:38:46 -0500

    hi

--- site/src/views/HomeView.vue
+++ site/src/views/HomeView.vue
@@ -2,18 +2,21 @@
 <div>
   <TopBar />
   <HomePage />
+  <Amp />
 </div>
 </template>
 
 <script>
 import HomePage from "../components/HomePage";
 import TopBar from "../components/TopBar"
+import Amp from "../components/Amp"
 export default {
   name: "Home",
 
   components: {
     HomePage,
-    TopBar
+    TopBar,
+    Amp
   },
 };
 </script>
