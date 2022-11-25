580430ba4f22e10a1fede8c450440792a918cb9f
nate <nateharrell@ksu.edu>
2022-11-25 13:35:57 -0600

    greeting

---
+++ site/src/components/Greeting.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <v-row>
+            <v-col cols="2"/>
+            <v-col cols="8">
+                Greeting
+            </v-col>
+            <v-col cols="2"/>
+        </v-row>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Greeting",    
+}
+</script>
+
+<style scoped>
+
+</style>
--- site/src/views/Home.vue
+++ site/src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
 <div>
     <Header/>
+    <Greeting/>
 </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Header from "../components/Header.vue"
+import Greeting from "../components/Greeting.vue"
 
 export default {
   name: "Home",
   components: {
     Header,
+    Greeting,
   }
 };
 </script>
