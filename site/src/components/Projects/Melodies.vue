dd7d347efb568f0989d5bf54b3fe179f5e9579fd
nate <nateharrell@ksu.edu>
2022-11-28 13:17:01 -0600

    hover

--- site/src/components/Projects/Melodies.vue
+++ site/src/components/Projects/Melodies.vue
@@ -13,18 +13,19 @@
   <div class="card">
     <div class="imgbox">
       <!-- <i class="fab fa-angular"></i> -->
-      <v-img src="@/assets/note.png" alt="nate"
+      <v-img src="@/assets/note.png" alt="nate" class="fab fa-angular"
       max-width="80"
-        class="nate">
+      >
       </v-img>
     </div>
     <div class="content">
       <h3 id="angular">
-        ANGULAR
+        MELODIES.TO
       </h3>
-      <p>Angular is a platform for building mobile
-        and desktop web applications
+      <p id="text-2">Melodies.to is a web application
+        that creates AI generated melodies.
       </p>
+      <a @click="toMelodies"> Visit </a>
     </div>
   </div>
 </div>
@@ -47,17 +48,6 @@ export default {
 }
 #text-2{
   background-image: linear-gradient(45deg, #eb1e82, #f88d30);
-  background-size: 100%;
-  font-weight:600;
-  font-size:26px;
-  background-clip: text;
-  -webkit-text-fill-color: transparent;
-}
-#text{
-  background-image: linear-gradient(45deg, #eb1e82, #f88d30);
-  background-size: 100%;
-  font-weight:300;
-  font-size:22px;
   background-clip: text;
   -webkit-text-fill-color: transparent;
 }
@@ -73,14 +63,14 @@ export default {
   display:flex;
   justify-content: space-around;
   flex-wrap: wrap;
-  padding:40px 0;
+  /* padding:40px 0; */
 }
 .card{
   position:relative;
   width:360px;
   height:210px; 
   background-color:#222;
-  margin: 40px 70px;
+  /* margin: 40px 70px; */
   border-radius:20px;
   box-shadow:0 5px 15px rgba(0,0,0, 0.5);
 }
@@ -101,5 +91,52 @@ export default {
   box-shadow:5px 5px 30px rgba(0,0,0,0.1);
   transition:0.5s ease-in-out;
   backdrop-filter: blur(20px);
+}
+.card:hover .imgbox{
+  width:150px;
+  height:150px;
+  left:-75px;
+  top:calc(50% - 75px);
+}
+.card .content{
+  position:absolute;
+  right: 0;
+  width:calc(100% - 75px);
+  height: 100%;
+  padding:28px;
+}
+#angular{
+  background-image: linear-gradient(45deg, #eb1e82, #f88d30);
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+.card .content a{
+  display:inline-block;
+  font-size:0.9em;
+  margin-top:10px;
+  border-radius:5px;
+  padding: 10px 20px;
+  text-decoration: none;
+  font-weight:400;
+  color:#fff;
+  background:linear-gradient(90deg, #f403d4, #f44141, #fce628, #f403d4);
+  background-size:400%;
+  z-index:1;
+}
+.card .content #angular{
+  color:#ff5165;
+  text-transform:uppercase;
+}
+.card .content a:hover{
+  animation: animate 8s linear infinite;
+}
+
+@keyframes animate{
+  0%{
+    background-position:0;
+  }
+  100%{
+    background-position:400%;
+  }
 }
 </style>
