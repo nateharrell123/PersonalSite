b7d8701e2aa1426729677f80eb4a8abbafe6b557
nate <nateharrell@ksu.edu>
2022-12-04 21:34:26 -0600

    hey

--- site/src/components/Boba/BobaFooter.vue
+++ site/src/components/Boba/BobaFooter.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="black">
+<div class="base">
     <v-row>
         <v-col class="header">
             Copyright © 2022 all rights reserved.
@@ -8,9 +8,37 @@
     <v-row>
         <v-col class="ml-8">
             <BobaAddress
+            :Location="'Mankato'"
             :Address="'711-2880 Nulla St. Mankato Mississippi 96522'"
             :Hours="'M-F, 10:30am - 8pm'"
             :Phone="'(257) 563-7401'"
+            :Email="'BobaTea@mail.com'"
+            />
+        </v-col>
+        <v-col class="ml-8">
+            <BobaAddress
+            :Location="'Roseville'"
+            :Address="'606-3727 Ullamcorper. Street Roseville NH 11523'"
+            :Hours="'M-F, 10:30am - 8pm'"
+            :Phone="'(786) 713-8616'"
+            :Email="'BobaTea@mail.com'"
+            />
+        </v-col>
+        <v-col class="ml-8">
+            <BobaAddress
+            :Location="'San Antonio'"
+            :Address="'7292 Dictum Av. San Antonio MI 47096'"
+            :Hours="'M-F, 10:30am - 8pm'"
+            :Phone="'(651) 233-3601'"
+            :Email="'BobaTea@mail.com'"
+            />
+        </v-col>
+        <v-col class="ml-8">
+            <BobaAddress
+            :Location="'Chelsea'"
+            :Address="'511-5762 At Rd. Chelsea MI 67708'"
+            :Hours="'M-F, 10:30am - 8pm'"
+            :Phone="'(128) 427-2631'"
             :Email="'BobaTea@mail.com'"
             />
         </v-col>
@@ -31,20 +59,28 @@ export default {
 </script>
 
 <style>
-.black{
-    background: #222;
+.base{
+    /* background: #222; */
+    background: #dc6f56;
     margin-top:80px;
-    height:353px;
+    height:370px;
+}
+@media (max-width:600px){
+    .base{
+        height:810px;
+    }
 }
 .body{
     color:white;
-    font-size:14px;
+    font-size:14px !important;
+    text-shadow: rgb(0 0 0 / 40%) 0px .1px 5px;
     letter-spacing:.5px;
 }
 .header{
     color:white;
     text-align:center;
     font-family: 'Passion One' serif;
-    font-size:12px;
+    /* text-shadow: rgb(0 0 0 / 40%) 0px 2px 5px; */
+    font-size:14px;
 }
 </style>
