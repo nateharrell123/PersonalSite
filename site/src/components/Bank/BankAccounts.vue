fe86a3a96a55ff28537958bd4803ee66682b0fe1
nate <nateharrell@ksu.edu>
2022-12-06 20:27:32 -0600

    push

---
+++ site/src/components/Bank/BankAccounts.vue
@@ -0,0 +1,61 @@
+<template>
+<div>
+    <v-row class="font mt-12 ml-12">
+        <v-col cols="8">
+            <span class="accounts">Accounts:</span>
+        </v-col>
+        <v-col>
+            <v-icon class="cog">mdi-cog-outline</v-icon>
+        </v-col>
+    </v-row>
+    <v-row>
+        <v-col cols="8" class="ml-16">
+        <v-card class="card"> <!--f7f7fa-->
+            <v-row>
+                <v-col class="ml-3 check">
+                    Checkings & Savings
+                </v-col>
+                <v-col align="end" class="mr-3 check">
+                    $224.44
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-col class="inner-col">
+                    hi
+                </v-col>
+            </v-row>
+        </v-card>
+        </v-col>
+    </v-row>
+</div>
+</template>
+
+<script>
+export default {
+    name: "BankAccounts",
+}
+</script>
+
+<style scoped>
+
+.font{
+  font-family: 'Montserrat', sans-serif;
+}
+.cog{
+    position:relative;
+    top:10px;
+    right:5px;
+    cursor:pointer;
+}
+.check{
+    border-bottom: .5px solid grey;
+}
+.card{
+}
+.inner-col{
+    
+}
+.accounts{
+    font-size:22px;
+}
+</style>
--- site/src/views/Bankview.vue
+++ site/src/views/Bankview.vue
@@ -1,15 +1,19 @@
 <template>
 <div>
     <BankHeader/>
+    <BankAccounts/>
 </div>
 </template>
 
 <script>
 import BankHeader from "../components/Bank/BankHeader.vue"
+import BankAccounts from "../components/Bank/BankAccounts.vue"
+
 export default {
     name: "Bankview",
     components: {
-        BankHeader
+        BankHeader,
+        BankAccounts
     }
 }
 </script>
