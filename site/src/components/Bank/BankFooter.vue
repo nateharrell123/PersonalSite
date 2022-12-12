b51ba4b65abad39e4a27f57cbed2e518ee8f1de3
nate <nateharrell@ksu.edu>
2022-12-12 15:39:59 -0600

    bugs

--- site/src/components/Bank/BankFooter.vue
+++ site/src/components/Bank/BankFooter.vue
@@ -80,7 +80,6 @@ export default {
 .header{
     color:white;
     text-align:center;
-    font-family: 'Passion One' serif;
     /* text-shadow: rgb(0 0 0 / 40%) 0px 2px 5px; */
     font-size:14px;
 }
